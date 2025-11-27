da764e03752383c6e09644977a435f529f4fb30f
Tamas <regoczi.tamas01@gmail.com>
2025-11-27 14:51:50 +0100

    Task 7 implement average temp measurement

--- src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
+++ src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
@@ -2,6 +2,21 @@ package com.aldisued.iot.monitoring.repository;
 
 import com.aldisued.iot.monitoring.entity.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SensorReadingRepository extends JpaRepository<SensorReading, String> {
+
+    @Query("""
+            SELECT reading FROM SensorReading reading
+            WHERE reading.sensor.type = 'TEMPERATURE'
+            AND
+            reading.timestamp >= :from
+            AND
+            reading.timestamp <= :to
+            """)
+    List<SensorReading> getTemperatureReadingsInTimePeriod(LocalDateTime from, LocalDateTime to);
+
 }
--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
@@ -1,8 +1,10 @@
 package com.aldisued.iot.monitoring.service;
 
+import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.entity.SensorType;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
 import java.time.LocalDateTime;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
@@ -23,8 +25,18 @@ public class MeasurementService {
   }
 
   public Optional<Double> getAverageTemperature(LocalDateTime from, LocalDateTime to) {
-    // TODO: Task 7
-    return Optional.empty();
-  }
 
+    List<SensorReading> readings = this.sensorReadingRepository.getTemperatureReadingsInTimePeriod(from, to);
+
+    if (readings.isEmpty()) {
+      return Optional.empty();
+    }
+
+    DoubleSummaryStatistics summaryStatistics = readings
+            .stream()
+            .mapToDouble(SensorReading::getValue)
+            .summaryStatistics();
+
+    return Optional.of(summaryStatistics.getAverage());
+  }
 }
