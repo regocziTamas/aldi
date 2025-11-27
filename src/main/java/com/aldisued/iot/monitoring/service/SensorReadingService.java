1bef7e8db77f88ab3959ce7ff9705bd619139188
Tamas <regoczi.tamas01@gmail.com>
2025-11-27 13:15:56 +0100

    Task 3 implement sensor reading save

--- src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
@@ -1,10 +1,13 @@
 package com.aldisued.iot.monitoring.service;
 
 import com.aldisued.iot.monitoring.dto.SensorReadingDto;
+import com.aldisued.iot.monitoring.entity.Sensor;
 import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
 import com.aldisued.iot.monitoring.repository.SensorRepository;
 import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
 
 @Service
 public class SensorReadingService {
@@ -19,8 +22,18 @@ public class SensorReadingService {
   }
 
   public SensorReading saveSensorReading(SensorReadingDto sensorReadingDto) {
-    //TODO: Task 3
-    return null;
-  }
 
+    Sensor sensor = this.sensorRepository.findById(sensorReadingDto.sensorId())
+            .orElseThrow(() -> new IllegalArgumentException(
+                    MessageFormat.format("Could not find sensor with id: {0}", sensorReadingDto.sensorId()))
+            );
+
+    SensorReading newSensorReading = new SensorReading(
+            sensorReadingDto.value(),
+            sensorReadingDto.timestamp(),
+            sensor
+    );
+
+    return sensorReadingRepository.save(newSensorReading);
+  }
 }
