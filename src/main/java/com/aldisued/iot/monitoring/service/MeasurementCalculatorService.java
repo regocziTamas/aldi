f2ec75060ac78832b17679edf10d33ac38fb06ea
Tamas <regoczi.tamas01@gmail.com>
2025-11-27 15:14:28 +0100

    Task 9 implement deviation filter of readings

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -1,20 +1,41 @@
 package com.aldisued.iot.monitoring.service;
 
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MeasurementCalculatorService {
 
   public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    // TODO: Task 9
-    return List.of();
+    if (deviation == null || deviation < 0.0 || deviation > 1.0) {
+      throw new IllegalArgumentException("Incorrect deviation value: " + deviation);
+    }
+
+    DoubleSummaryStatistics summaryStatistics = values
+            .stream()
+            .collect(Collectors.summarizingDouble(e -> e));
+
+    Double average = summaryStatistics.getAverage();
+    Double upperLimit = average + average * deviation;
+    Double lowerLimit = average - average * deviation;
+
+    return values
+            .stream()
+            .filter(val -> isWithinRange(val, upperLimit, lowerLimit))
+            .collect(Collectors.toList());
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
     // TODO: Task 10
     return List.of();
+  }
+
+  private boolean isWithinRange(Double val, Double upperLimit, Double lowerLimit) {
+    return val != null && val < upperLimit && val > lowerLimit;
   }
 
 }
